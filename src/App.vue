c4915973778d9909b652aa2c553059b258c122de
Marco Zellini <zellinimarco@gmail.com>
2023-09-28 16:54:45 +0200

    Fix Milestone 2 - Flags & Add Milestone 2 - Series

--- src/App.vue
+++ src/App.vue
@@ -15,26 +15,48 @@ export default {
   <div id="search_bar">
     <form>
       <label>Inserisci il titolo del film</label><br>
-      <input type="text" v-model="store.movieRequest.params.query">
+      <input type="text" v-model="store.inputQuery">
       <!-- <input type="text" v-model="store.movieRequest.params.query" v-model="store.movieRequest.params.query"> -->
       <button type="submit" @click.prevent="store.fetchData" placeholder="Cerca Titolo">Search</button>
     </form>
   </div>
   <div class="contents">
-    <ul v-if="store.movieList.length > 0">
-      <li v-for="movie in store.movieList">
-        <ol>
-          <li>{{ movie.title }}</li>
-          <li>{{ movie.originalTitle }}</li>
-          <li>
-            <img :src="movie.countryFlag">
-            {{ movie.language }}
-          </li>
-          <li>{{ movie.vote }}</li>
-        </ol>
-      </li>
-    </ul>
-    <div v-else>Non ci sono film! 😞</div>
+
+    <div class="film_list">
+      <h2>Films</h2>
+      <ul v-if="store.movieList.length > 0">
+        <li v-for="movie in store.movieList">
+          <ol>
+            <li>{{ movie.title }}</li>
+            <li>{{ movie.originalTitle }}</li>
+            <li>
+              <img :src="movie.countryFlag">
+              {{ movie.language }}
+            </li>
+            <li>{{ movie.vote }}</li>
+          </ol>
+        </li>
+      </ul>
+      <div v-else>Non ci sono film! 😞</div>
+    </div>
+
+    <div class="series_list">
+      <h2>Series</h2>
+      <ul v-if="store.seriesList.length > 0">
+        <li v-for="movie in store.seriesList">
+          <ol>
+            <li>{{ movie.title }}</li>
+            <li>{{ movie.originalTitle }}</li>
+            <li>
+              <img :src="movie.countryFlag">
+              {{ movie.language }}
+            </li>
+            <li>{{ movie.vote }}</li>
+          </ol>
+        </li>
+      </ul>
+      <div v-else>Non ci sono serie! 😞</div>
+    </div>
   </div>
   <!-- width="16" height="12"  -->
 </template>
--- src/store.js
+++ src/store.js
@@ -3,85 +3,85 @@ import axios from '../node_modules/axios'
 import { countryCodes } from './countryCodes.js'
 
 export const store = reactive({
-    movieRequest: { //this Object is used to make AJAX Calls to get our movie list
-        method: 'GET',
-        url: 'https://api.themoviedb.org/3/search/movie',
-        params: {
-            api_key: 'b5d1c2545fd5913c018a04b6cd2e056a',
-            query: ''
-        }
-    },
-    movieList: [], //list of movies,
+    baseUrl: 'https://api.themoviedb.org/3/search/',
+    api_key: 'b5d1c2545fd5913c018a04b6cd2e056a', //we need this to make AJAX request
+    inputQuery: null, //query
+    movieList: [], //list of movies found,
+    seriesList: [], //list of series found,
 
-    seriesRequest: {
-        method: 'GET', //this Object is used to make AJAX Calls to get our series list
-        url: 'https://api.themoviedb.org/3/search/tv',
-        params: {
-            api_key: 'b5d1c2545fd5913c018a04b6cd2e056a',
-            query: ''
-        }
+    /**
+     * ### fetchData
+     * > This function calls fetchMovies and fetchSeries functions
+     */
+    fetchData() {
+        this.fetchMovies();
+        this.fetchSeries();
     },
 
 
     /**
      * ### fetchMovies
-     * > This function does an AJAX Call to get a movie list bases on a research.
+     * > This function does an AJAX Call to get a movie list bases on a inputQuery.
      */
-    fetchData() {
-        if (this.movieRequest.params.query !== '') {
-            axios
-                .request(this.movieRequest)
-                .then((response) => {
+    fetchMovies() {
+        axios
+            .request({
+                method: 'GET',
+                url: this.baseUrl + 'movie',
+                params: {
+                    api_key: this.api_key,
+                    query: this.inputQuery
+                }
+            })
+            .then((response) => {
+                this.movieList = [];
 
-                    /* console.log(response);
-                    console.log(response.data.results[0]); */
-                    this.movieList = [];
-
-                    response.data.results.forEach(element => {
-                        this.movieList.push({
-                            title: element.title,
-                            originalTitle: element.original_title,
-                            language: element.original_language.toUpperCase(),
-                            countryFlag: `https://flagcdn.com/32x24/${countryCodes[element.original_language]}.png`,
-                            vote: element.vote_average
-                        })
-                    });
-
-                    // console.log(this.movieList);
-
-                }).catch((error) => {
-                    console.error(error);
+                response.data.results.forEach(element => {
+                    this.movieList.push({
+                        title: element.title,
+                        originalTitle: element.original_title,
+                        language: element.original_language.toUpperCase(),
+                        countryFlag: `https://flagcdn.com/32x24/${countryCodes[element.original_language]}.png`,
+                        vote: element.vote_average
+                    })
                 });
-        }
 
-        if (this.seriesRequest.params.query !== '') {
-            axios
-                .request(this.seriesRequest)
-                .then((response) => {
+            }).catch((error) => {
+                console.error(error);
+            });
+    },
 
-                    console.log(response);
-                    // console.log(response.data.results[0]);
-                    this.seriesList = [];
+    /**
+     * ### fetchMovies
+     * > This function does an AJAX Call to get a series list bases on a inputQuery.
+     */
+    fetchSeries() {
 
-                    response.data.results.forEach(element => {
-                        this.seriesList.push({
-                            title: element.title,
-                            originalTitle: element.original_title,
-                            language: element.original_language.toUpperCase(),
-                            countryFlag: `https://flagcdn.com/32x24/${countryCodes[element.original_language]}.png`,
-                            vote: element.vote_average
-                        })
-                    });
+        axios
+            .request({
+                method: 'GET', //this Object is used to make AJAX Calls to get our series list
+                url: this.baseUrl + 'tv',
+                params: {
+                    api_key: 'b5d1c2545fd5913c018a04b6cd2e056a',
+                    query: this.inputQuery
+                }
+            })
+            .then((response) => {
+                this.seriesList = [];
 
-                    console.log(this.seriesList);
-
-                }).catch((error) => {
-                    console.error(error);
+                response.data.results.forEach(element => {
+                    this.seriesList.push({
+                        title: element.name,
+                        originalTitle: element.original_name,
+                        language: element.original_language.toUpperCase(),
+                        countryFlag: `https://flagcdn.com/32x24/${countryCodes[element.original_language]}.png`,
+                        vote: element.vote_average
+                    })
                 });
-        }
+
+            }).catch((error) => {
+                console.error(error);
+            });
 
     }
-
-
-
 })
