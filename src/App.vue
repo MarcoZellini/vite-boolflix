da163c7792a4cbaab65d4aa4041e587cd2bd95aa
Marco Zellini <zellinimarco@gmail.com>
2023-09-28 14:12:11 +0200

    Add Milestone 1
    Get all the data we need from an AJAX call and insert them into the page

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,31 @@ export default {
       store
     }
   },
-  created() {
-    store.movieReqest.params.query = 'ritorno'
-    store.fetchMovies();
-  }
 }
 
 </script>
 
-<template></template>
+<template>
+  <div id="search_bar">
+    <form>
+      <label>Inserisci il titolo del film</label><br>
+      <input type="text" v-model="store.movieRequest.params.query">
+      <button type="submit" @click.prevent="store.fetchMovies" placeholder="Cerca Titolo">Search</button>
+    </form>
+  </div>
+  <div class="contents">
+    <ul v-if="store.movieList.length > 0">
+      <li v-for="movie in store.movieList">
+        <ol>
+          <li>{{ movie.title }}</li>
+          <li>{{ movie.originalTitle }}</li>
+          <li>{{ movie.language }}</li>
+          <li>{{ movie.vote }}</li>
+        </ol>
+      </li>
+    </ul>
+    <div v-else>Non ci sono film! 😞</div>
+  </div>
+</template>
 
 <style></style>
--- src/store.js
+++ src/store.js
@@ -2,10 +2,7 @@ import { reactive } from 'vue';
 import axios from '../node_modules/axios'
 
 export const store = reactive({
-    // baseMovieUrl: 'https://api.themoviedb.org/3/search/movie',
-    // apiKey: 'b5d1c2545fd5913c018a04b6cd2e056a',
-    // movieTitle: 'ritorno',
-    movieReqest: {
+    movieRequest: { //this Object is used to make AJAX Calls to get our movie list
         method: 'GET',
         url: 'https://api.themoviedb.org/3/search/movie',
         params: {
@@ -13,16 +10,36 @@ export const store = reactive({
             query: ''
         }
     },
+    movieList: [], //list of movies
 
-
+    /**
+     * ### fetchMovies
+     * > This function does an AJAX Call to get a movie list bases on a research.
+     */
     fetchMovies() {
-        axios
-            .request(this.movieReqest)
-            .then((response) => {
-                console.log(response);
-            }).catch((error) => {
-                console.error(error);
-            });
+        if (this.movieRequest.params.query !== '') {
+            axios
+                .request(this.movieRequest)
+                .then((response) => {
+
+                    /* console.log(response);
+                    console.log(response.data.results[0]); */
+
+                    response.data.results.forEach(element => {
+                        this.movieList.push({
+                            title: element.title,
+                            originalTitle: element.original_title,
+                            language: element.original_language,
+                            vote: element.vote_average
+                        })
+                    });
+
+                    console.log(this.movieList);
+
+                }).catch((error) => {
+                    console.error(error);
+                });
+        }
     }
 
 
